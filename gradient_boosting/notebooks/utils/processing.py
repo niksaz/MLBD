f5422b5bd962cb4e1d780bd1d4199eb790cc51f6
niksaz <nikitasazanovich@gmail.com>
2020-04-01 00:01:32 +0300

    Method implementation for CTR prediction

--- gradient_boosting/notebooks/utils/processing.py
+++ gradient_boosting/notebooks/utils/processing.py
@@ -12,7 +12,12 @@ def split_by_col(df, split_col, parts_fractions):
     for i in range(len(split_points) - 1):
         l = split_points[i]
         r = split_points[i + 1]
-        p = df.filter((F.lit(l) <= F.col(split_col)) & (F.col(split_col) < F.lit(r)))
+        if l == split_points[0]:
+            p = df.filter(F.col(split_col) < F.lit(r))
+        elif r == split_points[-1]:
+            p = df.filter(F.lit(l) <= F.col(split_col))
+        else:
+            p = df.filter((F.lit(l) <= F.col(split_col)) & (F.col(split_col) < F.lit(r)))
         parts.append(p)
 
     return parts
